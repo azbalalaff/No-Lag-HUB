f32f6eea5f022c1e35fdba6f4116f2a0b2de8f46
NoLag-id <sfpher6@gmail.com>
2025-08-20 13:36:42 +0800

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,2 +1,3 @@
 ID="ba003d4d-826a-4d61-b122-d0a0f793eec7";
 loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/azbalalaff/loading-screen/refs/heads/main/loader.lua"))()
